1758c96fe71f21b43cedf3b72d857182ddf887dd
stellalc7 <stellalc7@gmail.com>
2022-04-08 11:27:31 -0400

    backend to sol

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,3 +3,29 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+// import * as web3 from '@solana/web3.js';
+
+// (async () => {
+//   // Connect to cluster
+//   var connection = new web3.Connection(
+//     web3.clusterApiUrl('devnet'),
+//     'confirmed',
+//   );
+
+//   // Generate a new wallet keypair and airdrop SOL
+//   var wallet = web3.Keypair.generate();
+//   var airdropSignature = await connection.requestAirdrop(
+//     wallet.publicKey,
+//     web3.LAMPORTS_PER_SOL,
+//   );
+
+//   //wait for airdrop confirmation
+//   await connection.confirmTransaction(airdropSignature);
+
+//   // get account info
+//   // account data is bytecode that needs to be deserialized
+//   // serialization and deserialization is program specific
+//   let account = await connection.getAccountInfo(wallet.publicKey);
+//   console.log(account);
+// })();
